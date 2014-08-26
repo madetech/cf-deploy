9a69d7134297e6f6d44b89a8e5e055b920cc4ccb
Luke Morton <luke@madebymade.co.uk>
2014-08-26 14:28:05 +0100

    Replace coveralls with code climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,3 @@
-require 'simplecov'
-require 'coveralls'
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
-
-SimpleCov.start
