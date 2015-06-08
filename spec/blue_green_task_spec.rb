acd65bd4e7cbd4e94c2af14722797e69bad00e75
Richard Foster <richard@madetech.co.uk>
2015-06-08 17:18:08 +0100

    Use regular expressions to match cf-deploy routes in specs

--- spec/blue_green_task_spec.rb
+++ spec/blue_green_task_spec.rb
@@ -28,7 +28,7 @@ describe CF::Deploy do
     it 'should deploy blue if not currently deployed' do
       rake_tasks!
       expect(Kernel).to receive(:system).with('cf login').ordered
-      expect(IO).to receive(:popen).with("cf routes | grep 'www *yourwebsite.com'") { double(read: '', close: nil) }
+      expect(IO).to receive(:popen).with(/cf routes \| grep '(www|www-origin) \*yourwebsite.com'/) { double(read: '', close: nil) }
       expect(Kernel).to receive(:system).with('cf push -f manifests/production_blue.yml').and_return(true).ordered
       Rake::Task['cf:deploy:production'].invoke
     end
@@ -36,7 +36,7 @@ describe CF::Deploy do
     it 'should deploy blue if green currently deployed' do
       rake_tasks!
       expect(Kernel).to receive(:system).with('cf login').ordered
-      expect(IO).to receive(:popen).with("cf routes | grep 'www *yourwebsite.com'") { double(read: 'production-green-app', close: nil) }
+      expect(IO).to receive(:popen).with(/cf routes \| grep '(www|www-origin) \*yourwebsite.com'/) { double(read: 'production-green-app', close: nil) }
       expect(Kernel).to receive(:system).with('cf push -f manifests/production_blue.yml').and_return(true).ordered
       Rake::Task['cf:deploy:production'].invoke
     end
@@ -44,7 +44,7 @@ describe CF::Deploy do
     it 'should deploy green if blue currently deployed' do
       rake_tasks!
       expect(Kernel).to receive(:system).with('cf login').ordered
-      expect(IO).to receive(:popen).with("cf routes | grep 'www *yourwebsite.com'") { double(read: 'production-blue-app', close: nil) }
+      expect(IO).to receive(:popen).with(/cf routes \| grep '(www|www-origin) \*yourwebsite.com'/) { double(read: 'production-blue-app', close: nil) }
       expect(Kernel).to receive(:system).with('cf push -f manifests/production_green.yml').and_return(true).ordered
       Rake::Task['cf:deploy:production'].invoke
     end
